1583c1759c2a3b97e623cfa0dab4f00463a64043
Alex Lee <doomcat9167@gmail.com>
2017-05-16 07:47:21 -0700

    checkpoint at saving database

--- src/app/team-add/team-add.component.ts
+++ src/app/team-add/team-add.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { TeamService } from '../team.service';
+import { Team } from '../team.model';
 
 @Component({
   selector: 'app-team-add',
   templateUrl: './team-add.component.html',
-  styleUrls: ['./team-add.component.css']
+  styleUrls: ['./team-add.component.css'],
+  providers: [TeamService]
 })
 export class TeamAddComponent implements OnInit {
 
-  constructor() { }
+  constructor(private teamService: TeamService) { }
 
   ngOnInit() {
+  }
+
+  addPlayerDiv() {
+    console.log("Hello");
+    return "<h1>Hello</h1>"
+  }
+  submitForm(name: string, imgUrl: string, bio: string, players: any[]) {
+    var newTeam: Team = new Team(name, imgUrl, bio, players);
+    this.teamService.addTeam(newTeam);
+    console.log(newTeam);
   }
 
 }
